2ebf5f7cf0253c5d728625bb1a7c20405f87c861
41341759 <41341759@090690-P33S10.macklabs.br>
2016-04-01 19:30:51 -0300

    criado nova classe

---
+++ src/push/github/github.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package push.github;
+
+/**
+ *
+ * @author 41341759
+ */
+public class github {
+    private String git;
+    private String hub;
+}
